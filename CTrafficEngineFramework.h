7923e0987f1775bb0ae3515f186bd43ad8e7022b
emilw <emil@fornby.com>
2014-04-09 14:32:34 +0200

    First initial version of the Traffic engine in C++

---
+++ CTrafficEngineFramework.h
@@ -0,0 +1,16 @@
+//
+//  CTrafficEngineFramework.h
+//  TrafficEngine
+//
+//  Created by Emil Westholm on 2014-04-08.
+//  Copyright (c) 2014 Emil Westholm. All rights reserved.
+//
+
+#ifndef TrafficEngine_CTrafficEngineFramework_h
+#define TrafficEngine_CTrafficEngineFramework_h
+
+#include "CEngine.h"
+#include "CVehicle.h"
+
+
+#endif
